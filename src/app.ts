75fdc2ca365508e980458e9c16d0296a0b4b1340
MD Rakib Hasan <dev.rakibhasan1@gmail.com>
2026-02-09 17:20:03 +0600

    added better auth signIn with google add added secrets

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
 import cors from "cors";
 import { categoryRouter } from "./modules/category/category.router";
-import { tutorAvailabilityController } from "./modules/tutorSessionSlot/available.controller";
 import { bookingRouter } from "./modules/booking/booking.router";
 import { availableRouter } from "./modules/tutorSessionSlot/available.router";
 
--- src/modules/tutorProfile/profile.service.ts
+++ src/modules/tutorProfile/profile.service.ts
@@ -38,13 +38,44 @@ const createProfile = async (
 };
 
 const getProfile = async () => {
-  const result = await prisma.tutorProfile.findMany();
+  const result = await prisma.tutorProfile.findMany({
+    include: {
+      user: true, // includes all user info
+      categories: true, // all categories
+      reviews: {
+        include: {
+          user: true, // if Review has a relation to User
+        },
+      },
+      tutorAvailabilities: true,
+      bookings: {
+        where: { status: "confirmed" }, // optional, only upcoming or confirmed bookings
+        include: {
+          user: true,
+        },
+      },
+    },
+  });
   return result;
 };
 const getSingleProfile = async (tutorId: string) => {
   const result = await prisma.tutorProfile.findUnique({
-    where: {
-      id: tutorId,
+    where: { id: tutorId },
+    include: {
+      user: true, // includes all user info
+      categories: true, // all categories
+      reviews: {
+        include: {
+          user: true, // if Review has a relation to User
+        },
+      },
+      tutorAvailabilities: true,
+      bookings: {
+        where: { status: "confirmed" }, // optional, only upcoming or confirmed bookings
+        include: {
+          user: true,
+        },
+      },
     },
   });
   return result;
