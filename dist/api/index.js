29b408dd7b44a0f4102b7183549ce01eabf1f33a
MD Rakib Hasan <dev.rakibhasan1@gmail.com>
2026-03-13 21:44:42 +0600

    build app and it changes some codes

---
+++ dist/api/index.js
@@ -0,0 +1,18 @@
+import app from "../src/app";
+import { prisma } from "../src/lib/prisma";
+// Ensure DB connection
+async function main() {
+    try {
+        await prisma.$connect();
+        console.log("Connected to the database successfully!");
+    }
+    catch (error) {
+        console.error("An error occurred:", error);
+        await prisma.$disconnect();
+        process.exit(1);
+    }
+}
+main();
+// Export app as handler for Vercel
+export default app;
+//# sourceMappingURL=index.js.map
