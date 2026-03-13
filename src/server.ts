34e51ffba50f5f7ab1eb870ed13e308bbafa2865
MD Rakib Hasan <dev.rakibhasan1@gmail.com>
2026-03-13 21:25:53 +0600

    update server.ts file and vercel.json for deploy

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,12 @@
 import app from "./app";
 import { prisma } from "./lib/prisma";
 
-const PORT = process.env.PORT || 5000;
-
 async function main() {
   try {
     await prisma.$connect();
-    console.log("Connected to the database successfully.!");
-
-    app.listen(PORT, () => {
-      console.log("Database connected successfully on port :", PORT);
-    });
+    console.log("Connected to the database successfully!");
   } catch (error) {
-    console.log("An error occured :", error);
+    console.error("An error occurred:", error);
     await prisma.$disconnect();
     process.exit(1);
   }
@@ -20,5 +14,30 @@ async function main() {
 
 main();
 
-// Export the app for Vercel
+// Export only the app for Vercel
 export default app;
+
+// import app from "./app";
+// import { prisma } from "./lib/prisma";
+
+// const PORT = process.env.PORT || 5000;
+
+// async function main() {
+//   try {
+//     await prisma.$connect();
+//     console.log("Connected to the database successfully.!");
+
+//     app.listen(PORT, () => {
+//       console.log("Database connected successfully on port :", PORT);
+//     });
+//   } catch (error) {
+//     console.log("An error occured :", error);
+//     await prisma.$disconnect();
+//     process.exit(1);
+//   }
+// }
+
+// main();
+
+// // Export the app for Vercel
+// export default app;
